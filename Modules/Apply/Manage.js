c723d3032689838d479214632c8f29d4465d8bd2
funlake <funlake@163.com>
2015-01-31 23:00:59 +0000

    add apply management page

---
+++ Modules/Apply/Manage.js
@@ -0,0 +1,20 @@
+//User/Manage controller
+module.exports = function(app,database,config){
+	var common = require("../../Helpers/Common.js")();
+	return {
+		List : function(req,res){
+			//res.end("Welcome to easyparking!");
+			var Db = database.connect();
+			if(req.query.p == null || req.query.p <= 0){
+				req.query.p = 1;
+			}
+			var start  = (req.query.p-1) * 1 * config.DataLimit;
+			var result = Db.apply.find().sort({_id:-1}).skip(start).limit(config.DataLimit)
+			result.count(function(e,total){
+				result.toArray(function(error,result){
+					res.render("List",{data : result,pageNav : common.pageNav(total,req.query.p,config.DataLimit,req),req:req,res:res})
+				})
+			})
+		}
+	}
+}
